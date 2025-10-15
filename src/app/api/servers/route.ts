e72a4d40ab17b8e84ea425ed87c05a9c04bc4e97
Vijay <vijay@gmail.com>
2025-10-15 12:17:07 +0530

    add modal for creating channels

--- src/app/api/servers/route.ts
+++ src/app/api/servers/route.ts
@@ -60,3 +60,4 @@ export async function POST(req: Request) {
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
+
--- src/app/components/server-header.tsx
+++ src/app/components/server-header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ServerWithMembersWithProfiles } from "@/types";
-import { MemberRole, memberRoleEnum } from "../../../db/schema";
+import { MemberRole } from "../../../db/schema";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -67,7 +67,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className=" px-3 py-2 text-sm cursor-pointer flex items-center">
+          <DropdownMenuItem
+            className=" px-3 py-2 text-sm cursor-pointer flex items-center"
+            onClick={() => onOpen("createChannel")}
+          >
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto " />
           </DropdownMenuItem>
@@ -76,13 +79,19 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         {isModerator && <DropdownMenuSeparator />}
 
         {isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer flex items-center">
+          <DropdownMenuItem
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer flex items-center"
+            onClick={() => onOpen("deleteServer", { server })}
+          >
             Delete Server
             <Trash className="h-4 w-4 ml-auto " />
           </DropdownMenuItem>
         )}
         {!isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer flex items-center">
+          <DropdownMenuItem
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer flex items-center"
+            onClick={() => onOpen("leaveServer", { server })}
+          >
             Leave Server
             <LogOut className="h-4 w-4 ml-auto " />
           </DropdownMenuItem>
