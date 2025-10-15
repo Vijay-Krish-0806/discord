850d30ab4c7cc8c71c761623d62a67afabbf2d4d
Satyavathi Kota <satyavathi.kota@pal.tech>
2025-10-15 19:22:25 +0530

    Added channel navigation and chat header

--- src/app/me/(routes)/servers/[serverId]/page.tsx
+++ src/app/me/(routes)/servers/[serverId]/page.tsx
@@ -1,16 +1,37 @@
 import React from "react";
+import { getSession } from "../../../../../../lib/auth-utils";
+import { redirect, RedirectType } from "next/navigation";
+import db from "../../../../../../db/drizzle";
+interface ServerIdPageProps {
+  params:{
+    serverId:string;
+  }
+}
+const ServerIdPage = async ({params}:ServerIdPageProps) => {
+  const profile=await getSession();
+  if(!profile){
+    return redirect('/auth/sign-in');
+  }
+  
+  const server = await db.query.servers.findFirst({
+  where: (servers, { eq }) => eq(servers.id, params.serverId),
+  with: {
+    channels: {
+      where: (channels, { eq }) => eq(channels.name, "general"),
+      orderBy: (channels, { asc }) => [asc(channels.createdAt)]
+    },
+    members: {
+      where: (members, { eq }) => eq(members.userId, profile.id)
+    }
+  }
+}).then(server => 
+  server && server.members.length > 0 ? server : null
+);
+  const initialChannel=server?.channels[0];
+  if(initialChannel?.name!=='general')
+    return null;
 
-const ServerIdPage = () => {
-  return (
-    <div>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus dolores
-      quidem possimus qui, id reprehenderit et? Obcaecati asperiores minima
-      tempora, possimus labore dicta quo nulla repudiandae recusandae sunt
-      consequuntur numquam quasi dolorum inventore? Quod nam beatae, illum
-      libero obcaecati dolores quas! Facilis sequi non ipsam aperiam iusto hic,
-      rem eius!
-    </div>
-  );
+  return redirect(`/me/servers/${params.serverId}/channels/${initialChannel?.id}`);
 };
 
 export default ServerIdPage;
